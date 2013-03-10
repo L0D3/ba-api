f233279ba4a8ac68c53d6e96857e9bd79198948d
johannes lahann <johannes@jolalap.(none)>
2013-03-10 17:13:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaApi::Application.config.secret_token = 'af717204d33d5b736baaa8380656700d1d49980733dc6291340bc852c704e8adf481bd878f87d60383e93e8fc5d33e659fc98e1de9ee0494b5fbc284edf5b695'
